0c8721599df91f98ceb9a51128e0979208674e38
sanjadrinic <sanja.drinic@uns.ac.rs>
2022-05-30 17:07:23 +0200

    feat: validators for leaving company comments

--- agent-application-frontend/src/app/components/company-profile/dialog-comments/dialog-data.ts
+++ agent-application-frontend/src/app/components/company-profile/dialog-comments/dialog-data.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 export interface DialogData {
@@ -19,17 +20,38 @@ export interface DialogData {
       public dialogRef: MatDialogRef<DialogLeaveComment>,
       @Inject(MAT_DIALOG_DATA) public data: DialogData,
     ) {}
+    stars: number[] = [1, 2, 3, 4, 5];
+    selectedValue: number = 0;
+    star: number = -1;
+    formIsInvalid: boolean = true;
+    errorMessage: string = '';
+
+    titleFormControl = new FormControl('', [Validators.required]);
+    positionFormControl = new FormControl('', [Validators.required]);
+    contentFormControl = new FormControl('', [Validators.required]);
+    starsFormControl = new FormControl('', [Validators.min(1)]);
+    starsError: boolean = false;
   
     onNoClick(): void {
       this.dialogRef.close();
     }
-
-    stars: number[] = [1, 2, 3, 4, 5];
-    selectedValue: number = 0;
-    star: number = -1;
+    
     countStar(star) {
       this.selectedValue = star;
       this.data.rating = star
       console.log('Value of star', star);
+      this.starsError = false;
     }
+
+    checkIfFormIsInvalid(){
+      if(this.selectedValue == 0) {
+        this.starsError = true;
+      }
+      if(this.titleFormControl.hasError('required') || this.contentFormControl.hasError('required') || this.positionFormControl.hasError('required') || this.starsError) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
   }
