5eeb4cb12d69756e15ae9c4c93f654b358bec48f
Kristina Stojic <stojic.kris@gmail.com>
2022-06-01 20:05:36 +0200

    fix: deleted unnecessary function

--- agent-application-frontend/src/app/components/edit-company-info/edit-company-info.component.ts
+++ agent-application-frontend/src/app/components/edit-company-info/edit-company-info.component.ts
@@ -58,37 +58,6 @@ export class EditCompanyInfoComponent implements OnInit {
     }
   }
 
-  requestCompanyRegistration(){
-    this.request.userId = this.user.id;
-  
-    if (this.request.companyDTO.name.trim() != "" && this.request.companyDTO.description.trim() != "" && this.request.companyDTO.phoneNumber.trim() != ""){
-      
-      // broj telefona pocinje znakom '+', a zatim mogu biti samo cifre
-      if (this.request.companyDTO.phoneNumber[0] == "+"){
-        let isnum = /^\d+$/.test(this.request.companyDTO.phoneNumber.substring(this.request.companyDTO.phoneNumber.indexOf('+') + 1));
-        if (isnum) {
-          if (this.nameIsUnique(this.request.companyDTO.name)){
-            this.companyService.saveRegistrationRequest(this.request).subscribe(
-              (data: any) => {
-                alert("Request for company registation sent!")
-                this.dialogRef.close(); 
-              }
-            )
-          } else {
-            alert("Company name must be unique!");
-          }
-       } else {
-        alert("Wrong format for phone number!");
-       }
-      } else  {
-        alert("Wrong format for phone number!");
-      }
-    } else {
-      alert("All fields must be filled!");
-    }
-  }
-
-
   editCompanyInfo(){
 
     if (this.company.name.trim() != "" && this.company.description.trim() != "" && this.company.phoneNumber.trim() != ""){
